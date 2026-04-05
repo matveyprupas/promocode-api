5a507d9eeebea89b89bd6bccb0b2ef414d7d1f24
Matvey Prupas <matvey.prupas@1stdibs.com>
2026-04-05 15:40:01 +0300

    fix: install additional module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromocodesModule } from './promocodes/promocodes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PromocodesModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PromocodesModule],
   controllers: [AppController],
   providers: [AppService],
 })
