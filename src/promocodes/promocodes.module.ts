f9218f9f736953af954bd3f0cbe09f6496cf1f22
Matvey Prupas <matvey.prupas@1stdibs.com>
2026-04-05 15:28:28 +0300

    feat: add prisma module

--- src/promocodes/promocodes.module.ts
+++ src/promocodes/promocodes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { PromocodesController } from './promocodes.controller';
 import { PromocodesService } from './promocodes.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PromocodesController],
   providers: [PromocodesService],
 })
